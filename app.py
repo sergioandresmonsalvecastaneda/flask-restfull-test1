6b6cfdd423c256884dee2b24ff3dd3c33b78bab0
Sergio Monsalve <smonsalve@gmail.com>
2022-09-08 20:09:13 -0500

    mod wsgi

--- app.py
+++ app.py
@@ -9,6 +9,3 @@ class HelloWorld(Resource):
         return {'hello': 'world'}
 
 api.add_resource(HelloWorld, '/')
-
-if __name__ == '__main__':
-    app.run(debug=True)
--- wgsi.py
+++ wgsi.py
@@ -1,4 +1,5 @@
-from app.main import app
+from app import app
 
 if __name__ == "__main__":
-	app.run()
+	#app.run()
+	app.run(debug=True)
